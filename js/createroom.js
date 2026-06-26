5f3cd272ff019c3964ef19c09c40b90b885e2aeb
yuliandalaia <y.laia@students.amikom.ac.id>
2026-06-26 10:49:24 +0700

    ganti nama

--- js/createroom.js
+++ js/createroom.js
@@ -15,3 +15,34 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btnCreateRoom = document.getElementById('btnCreateRoom');
+    
+    const hostInput = document.getElementById('host-name');
+    const restaurantInput = document.getElementById('restaurant');
+    const rekeningInput = document.getElementById('rekening');
+
+    if (btnCreateRoom) {
+        btnCreateRoom.addEventListener('click', () => {
+            if (!hostInput || hostInput.value.trim() === "") {
+                alert("Peringatan: Host Name harus diisi!");
+                hostInput.focus();
+                return;
+            }
+
+            if (!restaurantInput || restaurantInput.value.trim() === "") {
+                alert("Peringatan: Restaurant Name harus diisi!");
+                restaurantInput.focus();
+                return; 
+            }
+
+            if (!rekeningInput || rekeningInput.value.trim() === "") {
+                alert("Peringatan: Nomor Rekening harus diisi!");
+                rekeningInput.focus();
+                return; 
+            }
+            window.location.href = 'room.html';
+        });
+    }
+});
